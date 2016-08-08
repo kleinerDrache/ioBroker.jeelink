eef530ebe54483e2901de151f54304c8524376c9
Tim Lieber <Timlieber75@gmail.com>
2016-08-08 19:17:06 +0200

    error fix

--- LaCrosse.js
+++ LaCrosse.js
@@ -2,18 +2,12 @@
 
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor
-var sp = new SerialPort(adapter.config.serialport, {baudrate:57600, parser: serialport.parsers.readline('\r\n')});
+var sp = new SerialPort("/dev/ttyUSB0", {baudrate:57600, parser: serialport.parsers.readline('\r\n')});
 
 
 var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
-
-
-// you have to call the adapter function and pass a options object
-// name has to be set and has to be equal to adapters folder name and main file name excluding extension
-// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
 var adapter = utils.adapter('LaCrosse');
 
-// is called when adapter shuts down - callback has to be called under any circumstances!
 adapter.on('unload', function (callback) {
     try {
         adapter.log.info('cleaned everything up...');
@@ -23,25 +17,18 @@ adapter.on('unload', function (callback) {
     }
 });
 
-// is called if a subscribed object changes
 adapter.on('objectChange', function (id, obj) {
-    // Warning, obj can be null if it was deleted
     adapter.log.debug('objectChange ' + id + ' ' + JSON.stringify(obj));
 });
 
-// is called if a subscribed state changes
 adapter.on('stateChange', function (id, state) {
-    // Warning, state can be null if it was deleted
     adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
 
-    // you can use the ack flag to detect if it is status (true) or command (false)
     if (state && !state.ack) {
         adapter.log.info('ack is not set!');
     }
 });
 
-// is called when databases are connected and adapter received configuration.
-// start here!
 adapter.on('ready', function () {
         adapter.log.info('entered ready');
     main();
@@ -53,9 +40,6 @@ function main() {
     serialport:     adapter.config.serialport || '/dev/ttyUSB0',
     baudrate:       adapter.config.baudrate   || 57600,
     };
-
-    // The adapters config (in the instance object everything under the attribute "native") is accessible via
-    // adapter.config:
 
     var obj = adapter.config.sensors;
     for (var anz in obj){
@@ -146,9 +130,9 @@ function main() {
                     // |---------------------- [0]fix "OK"
 
                  var tmp = data.split(' ');
-                if(tmp[0]==='OK'){      // Wenn ein Datensatz sauber gelesen wurde
-                    if(tmp[1]=='9'){    // Für jeden Datensatz mit dem fixen Eintrag 9
-                                        // somit werden alle SendorIDs bearbeitet
+                if(tmp[0]==='OK'){                      // Wenn ein Datensatz sauber gelesen wurde
+                    if(tmp[1]=='9'){                    // Für jeden Datensatz mit dem fixen Eintrag 9
+                                                        // somit werden alle SendorIDs bearbeitet
                         var tmpp=tmp.splice(2,6);       // es werden die vorderen Blöcke (0,1,2) entfernt
                         adapter.log.debug('splice       : '+ tmpp);
                         var buf = new Buffer(tmpp);
@@ -168,7 +152,5 @@ function main() {
             });
         }
     });
-
-    // in this template all states changes inside the adapters namespace are subscribed
     adapter.subscribeStates('*');
 }
